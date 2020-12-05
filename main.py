7fc86bf843861bd47f0c880f6eb258c390218832
rgoshen <rick.goshen@gmail.com>
2020-12-05 11:11:09 -0700

    updated birthdays.csv, & letters templates

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import pandas
+from datetime import datetime
+from random import randint
+import smtplib
 
-##################### Extra Hard Starting Project ######################
+MY_EMAIL = "novusterra6@gmail.com"
+MY_PASSWORD = "pflyers88"
 
-# 1. Update the birthdays.csv
+today_tuple = (datetime.now().month, datetime.now().day)
 
-# 2. Check if today matches a birthday in the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
 
-# 4. Send the letter generated in step 3 to that person's email address.
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-
-
-
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{contents}"
+        )
